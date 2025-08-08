a5d7115fc00cf36c9a845361a2897c5a31707046
OksKuskova <oskanchik.kuskova@gmail.com>
2025-08-08 11:31:33 +0300

    feat: Создаст основные компоненты: страницы catalog, product, basket, not-found и layout

---
+++ src/pages/catalog/catalog.tsx
@@ -0,0 +1,205 @@
+function Catalog(): JSX.Element {
+  return (
+    <>
+      <div className="banner">
+        <picture>
+          <source type="image/webp" srcSet="img/content/banner-bg.webp, img/content/banner-bg@2x.webp 2x"></source>
+          <img src="img/content/banner-bg.jpg" srcSet="img/content/banner-bg@2x.jpg 2x" width="1280" height="280" alt="баннер"> </img>
+        </picture>
+        <p className="banner__info"><span className="banner__message">Новинка!</span><span className="title title--h1">Cannonball&nbsp;Pro&nbsp;MX&nbsp;8i</span><span className="banner__text">Профессиональная камера от&nbsp;известного производителя</span><a className="btn" href="#">Подробнее</a></p>
+      </div>
+      <div className="page-content">
+        <div className="breadcrumbs">
+          <div className="container">
+            <ul className="breadcrumbs__list">
+              <li className="breadcrumbs__item">
+                <a className="breadcrumbs__link" href="index.html">Главная
+                  <svg width="5" height="8" aria-hidden="true">
+                    <use xlinkHref="#icon-arrow-mini"></use>
+                  </svg>
+                </a>
+              </li>
+              <li className="breadcrumbs__item"><span className="breadcrumbs__link breadcrumbs__link--active">Каталог</span>
+              </li>
+            </ul>
+          </div>
+        </div>
+        <section className="catalog">
+          <div className="container">
+            <h1 className="title title--h2">Каталог фото- и видеотехники</h1>
+            <div className="page-content__columns">
+              <div className="catalog__aside">
+                <div className="catalog-filter">
+                  <form action="#">
+                    <h2 className="visually-hidden">Фильтр</h2>
+                    <fieldset className="catalog-filter__block">
+                      <legend className="title title&#45;&#45;h5">Цена, ₽</legend>
+                      <div className="catalog-filter__price-range">
+                        <div className="custom-input">
+                          <label>
+                            <input type="number" name="price" placeholder="от"></input>
+                          </label>
+                        </div>
+                        <div className="custom-input">
+                          <label>
+                            <input type="number" name="priceUp" placeholder="до"></input>
+                          </label>
+                        </div>
+                      </div>
+                    </fieldset>
+                    <fieldset className="catalog-filter__block">
+                      <legend className="title title&#45;&#45;h5">Категория</legend>
+                      <div className="custom-radio catalog-filter__item">
+                        <label>
+                          <input type="radio" name="category" value="photocamera" checked><span className="custom-radio__icon"></span><span className="custom-radio__label">Фотокамера</span></input>
+                        </label>
+                      </div>
+                      <div className="custom-radio catalog-filter__item">
+                        <label>
+                          <input type="radio" name="category" value="videocamera"><span className="custom-radio__icon"></span><span className="custom-radio__label">Видеокамера</span></input>
+                        </label>
+                      </div>
+                    </fieldset>
+                    <fieldset className="catalog-filter__block">
+                      <legend className="title title&#45;&#45;h5">Тип камеры</legend>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="digital" checked><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Цифровая</span></input>
+                        </label>
+                      </div>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="film" disabled><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Плёночная</span></input>
+                        </label>
+                      </div>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="snapshot"><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Моментальная</span></input>
+                        </label>
+                      </div>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="collection" checked disabled><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Коллекционная</span></input>
+                        </label>
+                      </div>
+                    </fieldset>
+                    <fieldset className="catalog-filter__block">
+                      <legend className="title title&#45;&#45;h5">Уровень</legend>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="zero" checked><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Нулевой</span></input>
+                        </label>
+                      </div>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="non-professional"><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Любительский</span></input>
+                        </label>
+                      </div>
+                      <div className="custom-checkbox catalog-filter__item">
+                        <label>
+                          <input type="checkbox" name="professional"><span className="custom-checkbox__icon"></span><span className="custom-checkbox__label">Профессиональный</span></input>
+                        </label>
+                      </div>
+                    </fieldset>
+                    <button className="btn catalog-filter__reset-btn" type="reset">Сбросить фильтры
+                    </button>
+                  </form>
+                </div>
+              </div>
+              <div className="catalog__content">
+                <div className="catalog-sort">
+                  <form action="#">
+                    <div className="catalog-sort__inner">
+                      <p className="title title&#45;&#45;h5">Сортировать:</p>
+                      <div className="catalog-sort__type">
+                        <div className="catalog-sort__btn-text">
+                          <input type="radio" id="sortPrice" name="sort" checked></input>
+                          <label htmlFor="sortPrice">по цене</label>
+                        </div>
+                        <div className="catalog-sort__btn-text">
+                          <input type="radio" id="sortPopular" name="sort"></input>
+                          <label htmlFor="sortPopular">по популярности</label>
+                        </div>
+                      </div>
+                      <div className="catalog-sort__order">
+                        <div className="catalog-sort__btn catalog-sort__btn&#45;&#45;up">
+                          <input type="radio" id="up" name="sort-icon" checked aria-label="По возрастанию"></input>
+                          <label htmlFor="up">
+                            <svg width="16" height="14" aria-hidden="true">
+                              <use xlinkHref="#icon-sort"></use>
+                            </svg>
+                          </label>
+                        </div>
+                        <div className="catalog-sort__btn catalog-sort__btn&#45;&#45;down">
+                          <input type="radio" id="down" name="sort-icon" aria-label="По убыванию"></input>
+                          <label htmlFor="down">
+                            <svg width="16" height="14" aria-hidden="true">
+                              <use xlinkHref="#icon-sort"></use>
+                            </svg>
+                          </label>
+                        </div>
+                      </div>
+                    </div>
+                  </form>
+                </div>
+                <div className="cards catalog__cards">
+                  <div className="product-card">
+                    <div className="product-card__img">
+                      <picture>
+                        <source type="image/webp" srcSet="img/content/das-auge.webp, img/content/das-auge@2x.webp 2x"><img src="img/content/das-auge.jpg" srcSet="img/content/das-auge@2x.jpg 2x" width="280" height="240" alt="Ретрокамера «Das Auge IV»"></img></source>
+                      </picture>
+                    </div>
+                    <div className="product-card__info">
+                      <div className="rate product-card__rate">
+                        <svg width="17" height="16" aria-hidden="true">
+                          <use xlinkHref="#icon-full-star"></use>
+                        </svg>
+                        <svg width="17" height="16" aria-hidden="true">
+                          <use xlinkHref="#icon-full-star"></use>
+                        </svg>
+                        <svg width="17" height="16" aria-hidden="true">
+                          <use xlinkHref="#icon-full-star"></use>
+                        </svg>
+                        <svg width="17" height="16" aria-hidden="true">
+                          <use xlinkHref="#icon-star"></use>
+                        </svg>
+                        <svg width="17" height="16" aria-hidden="true">
+                          <use xlinkHref="#icon-star"></use>
+                        </svg>
+                        <p className="visually-hidden">Рейтинг: 3</p>
+                        <p className="rate__count"><span className="visually-hidden">Всего оценок:</span>23</p>
+                      </div>
+                      <p className="product-card__title">Ретрокамера «Das Auge IV»</p>
+                      <p className="product-card__price"><span className="visually-hidden">Цена:</span>73 450 ₽
+                      </p>
+                    </div>
+                    <div className="product-card__buttons">
+                      <button className="btn btn--purple product-card__btn" type="button">Купить
+                      </button>
+                      <a className="btn btn--transparent" href="#">Подробнее
+                      </a>
+                    </div>
+                  </div>
+                </div>
+                <div className="pagination">
+                  <ul className="pagination__list">
+                    <li className="pagination__item"><a className="pagination__link pagination__link&#45;&#45;active" href="1">1</a>
+                    </li>
+                    <li className="pagination__item"><a className="pagination__link" href="2">2</a>
+                    </li>
+                    <li className="pagination__item"><a className="pagination__link" href="3">3</a>
+                    </li>
+                    <li className="pagination__item"><a className="pagination__link pagination__link&#45;&#45;text" href="2">Далее</a>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </section>
+      </div>
+    </>
+  );
+}
+
+export default Catalog;
---
+++ src/pages/not-found/not-found.tsx
@@ -0,0 +1,12 @@
+function NotFound(): JSX.Element {
+  return (
+    <section className="catalog">
+      <div className="container">
+        <h1 className="title title--h2">404 Страница не найдена</h1>
+        <button className="btn btn--purple" type="button">Вернуться на главную</button>
+      </div>
+    </section>
+  );
+}
+
+export default NotFound;

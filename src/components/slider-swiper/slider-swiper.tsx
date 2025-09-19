1693764f204ad0faabf280d7feeee3e2ba03137a
OksKuskova <oskanchik.kuskova@gmail.com>
2025-09-19 13:00:47 +0300

    feat: Создаст универсальный компонент SliderSwiper

---
+++ src/components/slider-swiper/slider-swiper.tsx
@@ -0,0 +1,23 @@
+import 'swiper/css/bundle';
+
+import { ReactNode } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import type { SwiperProps } from 'swiper/react';
+import type { SwiperModule } from 'swiper/types';
+
+type SliderSwiperProps<T> = {
+  sliderParams?: SwiperProps,
+  modules?: SwiperModule[],
+  slides: T[];
+  renderSlide: (item: T, index: number) => ReactNode;
+}
+
+function SliderSwiper<T>({ sliderParams = {}, modules = [], slides, renderSlide }: SliderSwiperProps<T>): JSX.Element {
+
+  return (
+    < Swiper modules={modules} {...sliderParams} >
+      {slides.map((item: T, index: number) => <SwiperSlide key={index}>{renderSlide(item, index)} </SwiperSlide>)}
+    </ Swiper >
+  )
+}
+export default SliderSwiper;

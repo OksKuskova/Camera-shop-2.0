a513e157e43219ff73c105a1091207cf3bcf3cce
OksKuskova <oskanchik.kuskova@gmail.com>
2025-09-23 15:00:20 +0300

    feat: Добавит логику навигации и фокусировки для выпадающего списка элементов, используя :focus()

--- src/components/form-search/form-search.tsx
+++ src/components/form-search/form-search.tsx
@@ -1,15 +1,32 @@
 import './form-search.style.css';
 
-import { ChangeEvent, useState } from "react";
+import { ChangeEvent, KeyboardEvent, useEffect, useRef, useState } from "react";
 import { Camera } from "../../types/camera.types";
 import { getCamerasByName } from "../../mocks/cameras";
-import { Link } from "react-router-dom";
-import { getRoute } from "../../utils/utils.router";
 import { MIN_SEARCH_QUERY_LENGTH } from "./form-search.const";
+import { useNavigate } from 'react-router-dom';
+import { getRoute } from '../../utils/utils.router';
+import useArrayRefs from '../../hooks/use-array-refs';
 
 function FormSearch(): JSX.Element {
+  const navigate = useNavigate();
+
   const [searchQuery, setSearchQuery] = useState('');
   const [searchResults, setSearchResults] = useState<Camera[]>([]); // ! Думаю можно убрать state, используя useMemo, если не понадобится API
+  const [focusedElementIndex, setFocusedElementIndex] = useState<number>(-1);
+
+  const inputRef = useRef<HTMLInputElement | null>(null);
+  const buttonRef = useRef<HTMLButtonElement | null>(null);
+
+  const [itemRefs, setItemRef] = useArrayRefs<HTMLLIElement>();
+
+  console.log(focusedElementIndex);
+
+  useEffect(() => {
+    if (focusedElementIndex >= 0) {
+      itemRefs.current[focusedElementIndex]?.focus()
+    }
+  }, [focusedElementIndex]);
 
   const handleInputChange = (evt: ChangeEvent<HTMLInputElement>) => {
     const query = evt.target.value;
@@ -23,9 +40,62 @@ function FormSearch(): JSX.Element {
     }
   }
 
+  const handleInputKeyDown = (evt: KeyboardEvent<HTMLInputElement>) => {
+    const key = evt.key;
+
+    if (key === 'ArrowDown' && searchResults.length) {
+      evt.preventDefault();
+      setFocusedElementIndex((prev) => Math.min(prev + 1, searchResults.length - 1));
+    }
+  }
+
+  const handleItemKeyDown = (evt: KeyboardEvent<HTMLLIElement>, id: number, index: number) => {
+    const key = evt.key;
+
+    if (key === 'ArrowDown') {
+      evt.preventDefault();
+      setFocusedElementIndex((prev) => Math.min(prev + 1, searchResults.length - 1));
+    }
+
+    if (key === 'ArrowUp') {
+      evt.preventDefault();
+      setFocusedElementIndex((prev) => Math.max(prev - 1, 0));
+    }
+
+    if (key === 'Tab' && !evt.shiftKey) {
+      evt.preventDefault();
+      if (index < searchResults.length - 1) {
+        setFocusedElementIndex((prev) => Math.min(prev + 1, searchResults.length - 1));
+      } else {
+        buttonRef.current?.focus();
+        setFocusedElementIndex(-1);
+      }
+    }
+
+    if (key === 'Tab' && evt.shiftKey) {
+      evt.preventDefault();
+      if (focusedElementIndex === 0) {
+        inputRef.current?.focus();
+        setFocusedElementIndex(-1);
+      } else {
+        setFocusedElementIndex((prev) => Math.max(prev - 1, 0));
+      }
+    }
+
+    if (key === 'Enter') {
+      evt.preventDefault();
+      navigate(getRoute(id))
+    }
+  }
+
+  const handleMouseEnter = (index: number) => {
+    setFocusedElementIndex(index);
+  }
+
   const handleFormReset = () => {
     setSearchQuery('');
     setSearchResults([]);
+    setFocusedElementIndex(-1);
   }
 
   return (
@@ -35,20 +105,43 @@ function FormSearch(): JSX.Element {
           <svg className="form-search__icon" width="16" height="16" aria-hidden="true">
             <use xlinkHref="#icon-lens"></use>
           </svg>
-          <input className="form-search__input" type="text" autoComplete="off" placeholder="Поиск по сайту" value={searchQuery} onChange={(evt) => handleInputChange(evt)}></input>
+          <input
+            className="form-search__input"
+            type="text"
+            autoComplete="off"
+            placeholder="Поиск по сайту"
+            value={searchQuery}
+            onChange={(evt) => handleInputChange(evt)}
+            onKeyDown={(evt) => handleInputKeyDown(evt)}
+          >
+          </input>
         </label>
 
         {searchResults.length > 0 && (
           <ul className="form-search__select-list">
             {searchResults.map((product, index) => (
-              <li className="form-search__select-item" tabIndex={index} key={product.id} >
-                <Link tabIndex={-1} to={getRoute(product.id)}>{product.name}</Link>
-              </li>))}
+              <li
+                className="form-search__select-item"
+                tabIndex={0}
+                key={product.id}
+                ref={setItemRef(index)}
+                onClick={() => navigate(getRoute(product.id))}
+                onKeyDown={(evt) => handleItemKeyDown(evt, product.id, index)}
+                onMouseEnter={() => handleMouseEnter(index)}
+              >
+                {product.name}
+              </li>
+            ))}
           </ul>
         )}
       </form>
 
-      <button className="form-search__reset" type="button" onClick={handleFormReset}>
+      <button
+        className="form-search__reset"
+        type="button"
+        ref={buttonRef}
+        onClick={handleFormReset}
+      >
         <svg width="10" height="10" aria-hidden="true">
           <use xlinkHref="#icon-close"></use>
         </svg><span className="visually-hidden">Сбросить поиск</span>

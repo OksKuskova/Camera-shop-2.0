0e2976ba998f9e82afddc6a618560b12b244b429
OksKuskova <oskanchik.kuskova@gmail.com>
2025-10-20 13:56:50 +0300

    feat: Доработает хук use-lock-body-scroll, добавит таймаут для плавного возвращения скроллбара, обеспечит хранение данных о размере оригинального отступа и таймаут id в ref-контейнере, для своевременного сброса таймаута и предотвращения попадания в переменную originalPaddingRightRef компенсированного размера правого отступа при быстром включении - выключении модального окна

---
+++ src/components/modal/modal.const.ts
@@ -0,0 +1,1 @@
+export const MODAL_TRANSITION_TIME = 600;
--- src/hooks/use-lock-body-scroll.ts
+++ src/hooks/use-lock-body-scroll.ts
@@ -1,13 +1,21 @@
-import { useEffect } from "react";
+import { useEffect, useRef } from "react";
 import { BODY_SCROLL_LOCK } from "../constants/class-name";
+import { MODAL_TRANSITION_TIME } from "../components/modal/modal.const";
 
 export function useLockBodyScroll(isOpen: boolean) {
+  const originalPaddingRightRef = useRef<string>('');
+  const timeoutRef = useRef<ReturnType<typeof setTimeout> | null>(null);
+
   useEffect(() => {
     if (!isOpen) {
+
       return;
     }
 
-    const originalePaddingRight = document.body.style.paddingRight
+    if (!originalPaddingRightRef.current) {
+      originalPaddingRightRef.current = document.body.style.paddingRight;
+    }
+
     const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth;
 
     document.body.classList.add(BODY_SCROLL_LOCK);
@@ -17,8 +25,16 @@ export function useLockBodyScroll(isOpen: boolean) {
     }
 
     return () => {
-      document.body.classList.remove(BODY_SCROLL_LOCK);
-      document.body.style.paddingRight = `${originalePaddingRight}px`;
-    }
+      if (timeoutRef.current) {
+        clearTimeout(timeoutRef.current);
+      }
+
+      timeoutRef.current = setTimeout(() => {
+        document.body.classList.remove(BODY_SCROLL_LOCK);
+        document.body.style.paddingRight = originalPaddingRightRef.current;
+        timeoutRef.current = null;
+        originalPaddingRightRef.current = '';
+      }, MODAL_TRANSITION_TIME);
+    };
   }, [isOpen]);
 }

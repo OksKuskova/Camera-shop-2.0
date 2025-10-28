23d2c120742e18c6aa40ff376bb448bb418916cd
OksKuskova <oskanchik.kuskova@gmail.com>
2025-10-28 10:10:48 +0300

    feat: Добавит компонент Loader

---
+++ src/components/loader/loader.tsx
@@ -0,0 +1,11 @@
+import './loader.css';
+
+function Loader(): JSX.Element {
+  return (
+    <div className="loading-page">
+      <span className="loading-page__loader loader"></span>
+    </div>
+  )
+}
+
+export default Loader;

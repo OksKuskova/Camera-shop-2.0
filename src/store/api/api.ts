05fa57f78379112b6bf1595e335f9b2427870eee
OksKuskova <oskanchik.kuskova@gmail.com>
2025-10-29 13:34:33 +0300

    feat: Подключит обертку baseQueryWithErrorHandling для baseQuery

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -1,17 +1,19 @@
-import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
+import { createApi } from '@reduxjs/toolkit/query/react';
 import { Camera } from '../../types/camera.types';
 import { ApiRoute, ApiConfig } from './api.const';
+import { baseQueryWithErrorHandling } from './api.base-query-with-error-handling';
 
-const { PATH_NAME, BASE_URL } = ApiConfig;
+const { PATH_NAME } = ApiConfig;
 
 export const api = createApi({
   reducerPath: PATH_NAME,
-  baseQuery: fetchBaseQuery({ baseUrl: BASE_URL }),
+  baseQuery: baseQueryWithErrorHandling,
   endpoints: (build) => ({
     getCameras: build.query<Camera[], void>({
       query: () => ApiRoute.Cameras,
     }),
   }),
+
 })
 
 export const { useGetCamerasQuery } = api;

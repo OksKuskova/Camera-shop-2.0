f10ced82fa6e1190f3ccb1f79f01c9a399065f42
OksKuskova <oskanchik.kuskova@gmail.com>
2025-10-30 13:52:19 +0300

    feat(api): Добавит endpoint getSimilarProducts

--- src/store/api/api.ts
+++ src/store/api/api.ts
@@ -4,19 +4,23 @@ import { ApiRoute, ApiConfig } from './api.const';
 import { baseQueryWithErrorHandling } from './api.base-query-with-error-handling';
 
 const { PATH_NAME } = ApiConfig;
+const { Cameras, Similar } = ApiRoute;
 
 export const api = createApi({
   reducerPath: PATH_NAME,
   baseQuery: baseQueryWithErrorHandling,
   endpoints: (build) => ({
     getCameras: build.query<Camera[], void>({
-      query: () => ApiRoute.Cameras,
+      query: () => Cameras,
     }),
     getCameraById: build.query<Camera, number>({
-      query: (id) => `${ApiRoute.Cameras}/${id}`
-    })
+      query: (id) => `${Cameras}/${id}`
+    }),
+    getSimilarProducts: build.query<Camera[], number>({
+      query: (id) => `${Cameras}/${id}${Similar}`
+    }),
   }),
 
 })
 
-export const { useGetCamerasQuery, useGetCameraByIdQuery } = api;
+export const { useGetCamerasQuery, useGetCameraByIdQuery, useGetSimilarProductsQuery } = api;

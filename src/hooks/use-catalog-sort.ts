922aea7923464a5076a6ecf83261f379933b7652
OksKuskova <oskanchik.kuskova@gmail.com>
2025-11-07 15:51:20 +0300

    refactor: Инкапсулирует sort, setSort, handleChange и sortProducts, переместив логику сортировки со страницы Catalog в хук useCatalogSort

---
+++ src/hooks/use-catalog-sort.ts
@@ -0,0 +1,21 @@
+import { ChangeEvent, useMemo, useState } from "react";
+import { SORT_TYPE_DEFAULT, SORT_ORDER_DEFAULT } from "../components/form-sort/form-sort.const";
+import { Sort } from "../components/form-sort/form-sort.type";
+import { sortProducts } from "../components/form-sort/form-sort.utils";
+import { Camera } from "../types/camera.types";
+
+export function useCatalogSort(products: Camera[]) {
+  const [sort, setSort] = useState<Sort>({
+    type: SORT_TYPE_DEFAULT,
+    order: SORT_ORDER_DEFAULT,
+  })
+
+  const handleSortChange = (evt: ChangeEvent<HTMLInputElement>) => {
+    const { name, id } = evt.target;
+    setSort({ ...sort, [name]: id });
+  }
+
+  const sortedProducts = useMemo(() => sortProducts(products, sort), [products, sort]);
+
+  return { sort, handleSortChange, sortedProducts };
+}

83be6e62e2d0896ef32761c60c0ddf62c8207ae5
OksKuskova <oskanchik.kuskova@gmail.com>
2025-11-18 11:41:20 +0300

    feat: Добавит компонент FilterCategory

---
+++ src/components/form-filter/filter/filter-category.tsx
@@ -0,0 +1,24 @@
+import { CameraCategory } from "../../../constants/camera.const";
+import { useAppDispatch, useAppSelector } from "../../../store/hooks/store.index";
+import { getCategory, setCategory } from "../../../store/slices/filter-slice/filter-slice";
+import { CameraCategoryValue } from "../../../types/camera.types";
+import FilterItemBase from "../filter-item/filter-item-base";
+
+function FilterCategory(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  const currentCategory = useAppSelector(getCategory);
+
+  const handleInputChange = (category: CameraCategoryValue) => {
+    dispatch(setCategory(category));
+  }
+
+  return (
+    <>
+      {Object.entries(CameraCategory).map(([key, category]) => (
+        <FilterItemBase key={key} type='radio' name='category' label={category} value={key} checked={currentCategory === category} onChange={() => handleInputChange(category)} />))}
+    </>
+  )
+}
+
+export default FilterCategory;

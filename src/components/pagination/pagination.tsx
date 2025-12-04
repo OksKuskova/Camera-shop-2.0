cf16d31802e60bb4941fe9d05e279a04f50d3211
OksKuskova <oskanchik.kuskova@gmail.com>
2025-12-04 16:52:03 +0300

    feat: Изменит логику обработки клика по элементам Далее и Назад - теперь шаг не currentPage +/- 1, а отсчитывается от крайних значений текущего диапазона страниц

--- src/components/pagination/pagination.tsx
+++ src/components/pagination/pagination.tsx
@@ -1,6 +1,7 @@
 import { Link } from "react-router-dom";
 import { getPaginationItems } from "./pagination.utils";
-import { PaginationLabel } from "./pagination.const";
+import { PaginationItem } from "./pagination.type";
+import { DEFAULT_START_PAGE_NUMBER, PaginationLabel } from "./pagination.const";
 
 type PaginationProps = {
   totalPages: number,
@@ -9,20 +10,35 @@ type PaginationProps = {
 }
 
 function Pagination({ totalPages, currentPage, onPageChange }: PaginationProps): JSX.Element {
+
   const items = getPaginationItems(totalPages, currentPage);
+  const numericItems = items.filter((item) => typeof item === 'number');
+  const firstShowedItem = numericItems.length ? numericItems[0] : DEFAULT_START_PAGE_NUMBER;
+  const lastShowedItem = numericItems.length ? numericItems[numericItems.length - 1] : DEFAULT_START_PAGE_NUMBER;
 
   return (
     <div className="pagination">
       <ul className="pagination__list">
         {items.map((item) => {
           const extraClassName = typeof item === 'number' ? currentPage === item ? 'pagination__link--active' : '' : 'pagination__link--text';
-          const handleClick = () => onPageChange(typeof item === 'number' ? item : (item === PaginationLabel.prev ? currentPage - 1 : currentPage + 1));
+          const handleClick = (item: PaginationItem) => {
+            if (item === PaginationLabel.next) {
+              onPageChange(lastShowedItem + 1);
+              return;
+            }
+            if (item === PaginationLabel.prev) {
+              onPageChange(firstShowedItem - 1);
+              return;
+            }
+            return onPageChange(item);
+          }
 
           return (<li key={item} className="pagination__item">
             <Link className={`pagination__link ${extraClassName}`}
               to=''
-              onClick={handleClick}
-            >{item}</Link>
+              onClick={() => handleClick(item)}
+            >{item}
+            </Link>
           </li>)
         })}
       </ul>
